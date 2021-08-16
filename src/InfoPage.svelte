01f6c480cb45d8d794f2e687913773fb9dd39b06
Andreas Rozek <A.Rozek@gmx.de>
2021-08-16 17:53:46 +0200

    Create InfoPage.svelte

---
+++ src/InfoPage.svelte
@@ -0,0 +1,34 @@
+<style>
+  .InfoPage {
+    display:flex; flex-flow:column nowrap; align-items:stretch;
+    display:relative; width:100%; height:100%;
+  }
+
+  .NavigationBar {
+    display:flex; flex:0 0 auto;
+    width:100%; height:44px;
+    border:none; border-bottom:solid 1px black;
+    font-size:18px; font-weight:bold;
+  }
+
+  .NavigationBarTitle {
+    display:block; position:absolute;
+    width:100%; height:44px;
+    text-align:center; line-height:44px;
+    pointer-events:none;
+  }
+
+  .ContentArea {
+    display:block; flex:1 1 auto;
+  }
+</style>
+
+<div class="InfoPage">
+  <div class="NavigationBar">
+    <div class="NavigationBarTitle">VfB-Notes</div>
+  </div>
+  <div class="ContentArea">
+  </div>
+
+  <slot></slot>
+</div>
