951949cfbfb38ba25bf01804fef4a5f2500586fc
Andreas Rozek <A.Rozek@gmx.de>
2021-08-17 14:32:59 +0200

    Create ResetRequestDialog.svelte

---
+++ src/ResetRequestDialog.svelte
@@ -0,0 +1,156 @@
+<style>
+  .Dialog {
+    display:inline-block; flex:0 0 auto; position:relative;
+    width:300px; height:auto;
+    margin:0px; margin-top:-20px;
+    border:none; border-radius:8px;
+    padding:10px;
+    box-shadow:0px 0px 5px 5px rgba(0,0,0,0.2);
+    background-color:white;
+  }
+
+  .Dialog a, .Dialog a:visited {
+    color:#2980B9;
+    text-decoration:underline;
+  }
+
+  .Dialog > div {
+    display:flex; position:relative;
+    flex-flow:column nowrap; align-items:stretch;
+    border:solid 2px lightgray; border-radius:4px;
+    padding:10px;
+  }
+
+  .Dialog > div > [name="CloseButton"] {
+    display:block; position:absolute;
+    top:-20px; right:-20px; width:20px; height:20px;
+    border:solid 2px white; border-radius:50%;
+    box-shadow:0px 0px 5px 5px rgba(0,0,0,0.2), 0px 0px 0px 1px lightgray;
+    background-color:black;
+    padding:0px;
+    font-size:18px; font-weight:bold; line-height:12px;
+    text-align:center;
+    color:white;
+    cursor:pointer;
+  }
+
+  .Dialog > div > [name="Title"] {
+    display:block; position:relative;
+    padding:4px 0px 14px 0px;
+    font-size:18px; font-weight:bold;
+    text-align:center;
+    color:#222222;
+  }
+
+  .Dialog > div > .Block {
+    display:block; margin:0px 0px 10px 0px;
+    text-align:justify;
+  }
+
+  .Dialog > div > input {
+    appearance:none; -webkit-appearance:none; -moz-appearance:none; -o-appearance:none;
+    display:block; position:relative;
+    margin:4px 0px 4px 0px; padding:4px;
+    border:solid 1px lightgray; border-radius:2px;
+    font-size:16px;
+  }
+
+  .Dialog > div > button {
+    appearance:none; -webkit-appearance:none; -moz-appearance:none; -o-appearance:none;
+    display:block; position:relative;
+    margin:4px 0px 4px 0px; padding:6px;
+    background-color:#2980B9;
+    border:none; border-radius:4px;
+    font-size:16px; font-weight:bold; color:white;
+    cursor:pointer;
+  }
+
+  .Dialog > div > button:disabled {
+    opacity:0.3;
+    cursor:auto;
+  }
+
+  .Dialog > div > .FormMessage {
+    display:inline-block; position:relative;
+    left:2px; top:-2px;
+    font-size:12px
+  }
+
+  .Dialog > div > .invalid.FormMessage {
+    color:red;
+  }
+
+
+</style>
+
+<script context="module" lang="ts">
+  import { ValueIsEMailAddress } from 'javascript-interface-library'
+
+  import {
+    focusOnApplication, startPasswordResetForCustomer
+  } from 'voltcloud-for-browsers'
+
+  import { Globals } from './Globals.js'
+</script>
+
+<script lang="ts">
+  let EMailAddress:string, AddressLooksBad:boolean,  AddressMessage:string
+
+  EMailAddress = $Globals.EMailAddress || ''
+
+  $: switch (true) {
+    case (EMailAddress.trim() === ''):
+      AddressLooksBad = true;  AddressMessage = 'please, enter your EMail address'
+      break
+    case ValueIsEMailAddress(EMailAddress):
+      AddressLooksBad = false; AddressMessage = 'your email address looks acceptable'
+      break
+    default:
+      AddressLooksBad = true;  AddressMessage = 'please, enter a valid EMail address'
+  }
+
+  $: SubmitIsForbidden = AddressLooksBad
+
+  function closeDialog (Event) {
+    Event.preventDefault()
+    Globals.define('State','')
+  }
+
+  async function submitRequest (Event) {
+    Event.preventDefault()
+    Globals.define({ State:'requestingReset' })
+
+    try {
+      await focusOnApplication($Globals.ApplicationURL,$Globals.ApplicationId)
+      await startPasswordResetForCustomer(EMailAddress)
+    } catch (Signal) {
+      Globals.define({ State:'CommunicationFailure', FailureReason:Signal.toString() })
+      return
+    }
+
+    Globals.define({ State:'ResetRequested' })
+  }
+</script>
+
+<div class="Dialog">
+  <div>
+    <div name="CloseButton" on:click={closeDialog}>&times;</div>
+    <div name="Title">Request Password Reset</div>
+
+    <div class="Block">
+      If you forgot your password, you may ask for an email containing a link
+      which will allow you to define a new password.
+    </div>
+
+    <div class="Block">
+      That email will be sent immediately after submitting this request, the link
+      within will be valid for one hour.
+    </div>
+
+    <input name="EMailAddressInput" type="email" bind:value={EMailAddress}
+      placeholder="your email address">
+    <div class:FormMessage={true} class:invalid={AddressLooksBad}>{AddressMessage}</div>
+
+    <button name="SubmitButton" disabled={SubmitIsForbidden} on:click={submitRequest}>SubmitRequest</button>
+  </div>
+</div>
