06926eb11f073b8c891345447d61a77c9f9224aa
Andreas Rozek <A.Rozek@gmx.de>
2021-08-17 08:13:32 +0200

    Create Overlay.svelte

---
+++ src/Overlay.svelte
@@ -0,0 +1,12 @@
+<style>
+  .Overlay {
+    display:flex; flex-flow:column nowrap; justify-content:center; align-items:center;
+    position:absolute; left:0px; top:0px; width:100%; height:100%;
+    background-color:rgb(0,0,0,0.2);
+    pointer-events:none;
+  }
+</style>
+
+<div class="Overlay">
+  <slot></slot>
+</div>
