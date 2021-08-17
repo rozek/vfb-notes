0464c3ebf9be656735b16ccea60ffb0b784959c6
Andreas Rozek <A.Rozek@gmx.de>
2021-08-17 09:36:04 +0200

    Update Overlay.svelte

--- src/Overlay.svelte
+++ src/Overlay.svelte
@@ -3,7 +3,6 @@
     display:flex; flex-flow:column nowrap; justify-content:center; align-items:center;
     position:absolute; left:0px; top:0px; width:100%; height:100%;
     background-color:rgb(0,0,0,0.2);
-    pointer-events:none;
   }
 </style>
 
