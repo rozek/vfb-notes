3719a550207f8767350cded475929617058e2f15
Andreas Rozek <A.Rozek@gmx.de>
2021-08-17 09:35:53 +0200

    Update Globals.js

--- src/Globals.js
+++ src/Globals.js
@@ -1,16 +1,45 @@
   import { writable } from 'svelte/store'
 
   const { subscribe, set, update } = writable({
-    AccessToken: undefined,
-    State:       undefined
+    AccessToken:       undefined,
+    ConfirmationToken: undefined,
+    ResetToken:        undefined,
+    EMailAddress:      undefined,
+    Password:          undefined,
+    State:             undefined
   })
 
   function define (KeyOrObject, Value) {
     if (typeof(KeyOrObject) === 'string') {
       update((Globals) => { Globals[KeyOrObject] = Value; return Globals })
+      switch (KeyOrObject) {
+        case 'AccessToken': sessionStorage['vfb-notes: access-token']  = Value; break
+        case 'EMailAddress':  localStorage['vfb-notes: email-address'] = Value; break
+        case 'Password':    sessionStorage['vfb-notes: password']      = Value
+      }
     } else {
       update((Globals) => Object.assign(Globals,KeyOrObject))
+
+      if ('AccessToken' in KeyOrObject) {
+        sessionStorage['vfb-notes: access-token'] = KeyOrObject['AccessToken']
+      }
+
+      if ('EMailAddress' in KeyOrObject) {
+        localStorage['vfb-notes: email-address'] = KeyOrObject['EMailAddress']
+      }
+
+      if ('Password' in KeyOrObject) {
+        sessionStorage['vfb-notes: password'] = KeyOrObject['Password']
+      }
     }
   }
+
+  define({
+    AccessToken:       sessionStorage['vfb-notes: access-token'] || '',
+    ConfirmationToken: '',
+    ResetToken:        '',
+    EMailAddress:      localStorage['vfb-notes: email-address']  || '',
+    Password:          sessionStorage['vfb-notes: password']     || ''
+  })
 
   export const Globals = { subscribe, define }
