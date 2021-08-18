b099bdc29294c3931c79bce5b4c0477aea4fcbbe
Andreas Rozek <A.Rozek@gmx.de>
2021-08-18 10:49:13 +0200

    Create PasswordResetNotice.svelte

---
+++ src/PasswordResetNotice.svelte
@@ -0,0 +1,39 @@
+<style>
+  .Dialog {
+    display:inline-block; flex:0 0 auto; position:relative;
+    width:300px; height:auto;
+    margin:0px; margin-top:-20px;
+    border:none; border-radius:8px;
+    padding:10px;
+    box-shadow:0px 0px 5px 5px rgba(0,0,0,0.2);
+    background-color:white;
+  }
+
+  .Dialog a, .Dialog a:visited {
+    color:#2980B9;
+    text-decoration:underline;
+  }
+
+  .Dialog > div {
+    display:flex; position:relative;
+    flex-flow:column nowrap; align-items:stretch;
+    border:solid 2px lightgray; border-radius:4px;
+    padding:10px;
+  }
+
+  .Dialog > div > [name="Title"] {
+    display:block; position:relative;
+    padding:4px 0px 14px 0px;
+    font-size:18px; font-weight:bold;
+    text-align:center;
+    color:#222222;
+  }
+
+
+</style>
+
+<div class="Dialog">
+  <div>
+    <div name="Title">Password is being reset...</div>
+  </div>
+</div>
