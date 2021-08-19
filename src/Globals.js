3ec6d299a4f073ca674008bb9caa2c2639925875
Andreas Rozek <A.Rozek@gmx.de>
2021-08-19 10:59:43 +0200

    Update Globals.js

--- src/Globals.js
+++ src/Globals.js
@@ -5,11 +5,11 @@
   const { subscribe, set, update } = writable({
     ApplicationURL:    'https://vfb-notes.volt.live/',
     ApplicationId:     'd3CX6D',
-    AccessToken:       sessionStorage['vfb-notes: access-token'] || '',
+    AccessToken:       '',
     ConfirmationToken: '',
     ResetToken:        '',
     EMailAddress:      localStorage['vfb-notes: email-address']  || '',
-    Password:          sessionStorage['vfb-notes: password']     || '',
+    Password:          '',
     loggedIn:          false,
     firstName:         '',
     lastName:          '',
@@ -22,9 +22,8 @@
     if (typeof(KeyOrObject) === 'string') {
       update((Globals) => { Globals[KeyOrObject] = Value; return Globals })
       switch (KeyOrObject) {
-        case 'AccessToken': sessionStorage['vfb-notes: access-token']  = Value; break
         case 'EMailAddress':  localStorage['vfb-notes: email-address'] = Value; break
-        case 'Password':    sessionStorage['vfb-notes: password']      = Value
+        case 'Password':
           let PasswordHash  = hash(new TextEncoder().encode(Value))
           let EncryptionKey = PasswordHash.slice(0,secretbox.keyLength)
           update((Globals) => { Globals['EncryptionKey'] = EncryptionKey; return Globals })
@@ -32,17 +31,11 @@
     } else {
       update((Globals) => Object.assign(Globals,KeyOrObject))
 
-      if ('AccessToken' in KeyOrObject) {
-        sessionStorage['vfb-notes: access-token'] = KeyOrObject['AccessToken']
-      }
-
       if ('EMailAddress' in KeyOrObject) {
         localStorage['vfb-notes: email-address'] = KeyOrObject['EMailAddress']
       }
 
       if ('Password' in KeyOrObject) {
-        sessionStorage['vfb-notes: password'] = KeyOrObject['Password']
-
         let PasswordHash  = hash(new TextEncoder().encode(KeyOrObject['Password']))
         let EncryptionKey = PasswordHash.slice(0,secretbox.keyLength)
         update((Globals) => { Globals['EncryptionKey'] = EncryptionKey; return Globals })
