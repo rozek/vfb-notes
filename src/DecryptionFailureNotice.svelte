69099e8e82ca52d6b89937a864cae030b0eea6c9
Andreas Rozek <A.Rozek@gmx.de>
2021-08-19 08:48:58 +0200

    Create DecryptionFailureNotice.svelte

---
+++ src/DecryptionFailureNotice.svelte
@@ -0,0 +1,80 @@
+<style>
+  .Dialog {
+    display:inline-block; flex:0 0 auto; position:relative;
+    width:300px; height:auto;
+    margin:0px; margin-top:-20px;
+    border:none; border-radius:8px;
+    padding:10px;
+    box-shadow:0px 0px 5px 5px rgba(0,0,0,0.2);
+    background-color:white;
+  }
+
+  .Dialog > div {
+    display:flex; position:relative;
+    flex-flow:column nowrap; align-items:stretch;
+    border:solid 2px lightgray; border-radius:4px;
+    padding:10px;
+  }
+
+  .Dialog > div > [name="Title"] {
+    display:block; position:relative;
+    padding:4px 0px 14px 0px;
+    font-size:18px; font-weight:bold;
+    text-align:center;
+    color:#222222;
+  }
+
+
+  .Dialog > div > .Block {
+    display:block; margin:0px 0px 10px 0px;
+    text-align:justify;
+  }
+
+
+  .Dialog > div > button {
+    appearance:none; -webkit-appearance:none; -moz-appearance:none; -o-appearance:none;
+    display:block; position:relative;
+    margin:4px 0px 4px 0px; padding:6px;
+    background-color:#2980B9;
+    border:none; border-radius:4px;
+    font-size:16px; font-weight:bold; color:white;
+    cursor:pointer;
+  }
+
+  .Dialog > div > button:disabled {
+    opacity:0.3;
+    cursor:auto;
+  }
+
+
+</style>
+
+<script context="module" lang="ts">
+  import { Globals } from './Globals.js'
+</script>
+
+<script lang="ts">
+  function closeNotice () {
+    Globals.define('State','')
+  }
+</script>
+
+<div class="Dialog">
+  <div>
+    <div name="Title">Decryption Failure</div>
+
+    <div class="Block">
+      The note text found on VoltCloud could not be decrypted. Such a situation
+      may occur after a password reset or a password change from one browser
+      followed by a save from another one.
+    </div>
+
+    <div class="Block">
+      If you know the password with which the text was encrypted, you may try
+      to log-out and then to reset(!) your password. Important: do not
+      just "change" your password, that will not help!
+    </div>
+
+    <button on:click|preventDefault={closeNotice}>Ok</button>
+  </div>
+</div>
